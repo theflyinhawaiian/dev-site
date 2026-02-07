8915cb7bc99f568c8dacbd5486cc7abb850d4611
Peter Mullins <jc310xc@gmail.com>
2026-02-07 01:23:03 -0500

    get hot reload working

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3001',
