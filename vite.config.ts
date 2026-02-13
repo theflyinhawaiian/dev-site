5c24236c0db3dd509d73156bc33043ba7a1a282d
Peter Mullins <jc310xc@gmail.com>
2026-02-13 01:42:56 -0500

    docker config tweaks for prod

--- vite.config.ts
+++ vite.config.ts
@@ -22,5 +22,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    allowedHosts: [
+      "pmullins.dev",
+      "www.pmullins.dev",
+      "localhost"
+    ]
   },
 });
